f193e012da9daae5c7cc783f126dec7b15a945cc
Farkhod <sadykovfarkhod@gmail.com>
2020-03-10 01:23:08 -0500

    Created example data types

---
+++ dataTypes.playground/Contents.swift
@@ -0,0 +1,27 @@
+// String data type
+let my_message = "Hello World!"
+
+// Integer data type
+let my_age = 25
+
+// Float point
+let my_money = 24.12
+
+let amIrich = true
+
+// Array in Swift
+let my_list = ["Farkhod", "Nazira", "Farrukh"]
+
+// Creating Dictionary in Swift
+var my_info = ["first_name": "Farkhod", "last_name": "Sadykov"]
+
+
+// Show all data types
+print(my_message)
+print(my_age)
+print(my_money)
+print(amIrich)
+print(my_list)
+print(my_info)
+
+
